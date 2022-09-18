25b068e1e1536aee048306c9dd7715b6ab579291
Jin Kang <kbjss071@gmail.com>
2022-09-17 21:24:40 -0400

    finished working on integrating dynamoDB apis

---
+++ server/utils/params-config.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split('.');
+  const fileType = myFile[myFile.length - 1];
+  
+  const imageParams = {
+    // Replace the <My_Bucket_Name> with the name of your own S3 bucket
+    Bucket: '	user-images-0294d6d2-9bf8-44b0-9d60-696b49e1a26d',
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+  
+  return imageParams;
+};
+
+module.exports = params;
